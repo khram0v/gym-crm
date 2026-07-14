443f7d36a5d25e233d7450bdc7307441a558f1c5
Sviatoslav Khramov <kent8352@gmail.com>
2026-07-14 21:33:32 +0300

    Add JPA entities

---
+++ src/main/java/io/github/khram0v/gymcrm/model/TrainingType.java
@@ -0,0 +1,41 @@
+package io.github.khram0v.gymcrm.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "training_types")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TrainingType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "training_type_name", nullable = false, unique = true)
+    private String trainingTypeName;
+
+    public TrainingType(String trainingTypeName) {
+        this.trainingTypeName = trainingTypeName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TrainingType that = (TrainingType) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
