819b793e0a309a50c9521b3fe052892ad1190817
Sviatoslav Khramov <kent8352@gmail.com>
2026-07-28 10:52:59 +0300

    Remove wildcards from imports

--- src/main/java/io/github/khram0v/gymcrm/model/Trainer.java
+++ src/main/java/io/github/khram0v/gymcrm/model/Trainer.java
@@ -1,6 +1,13 @@
 package io.github.khram0v.gymcrm.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
