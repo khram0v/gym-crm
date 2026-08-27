66c34323f84352ae5106ed6aabf587e1b3dda99c
Sviatoslav Khramov <kent8352@gmail.com>
2026-08-27 14:03:15 +0300

    Fix Eureka registration

--- src/main/java/io/github/khram0v/gymcrm/client/config/RestClientConfig.java
+++ src/main/java/io/github/khram0v/gymcrm/client/config/RestClientConfig.java
@@ -3,6 +3,7 @@ package io.github.khram0v.gymcrm.client.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
@@ -10,12 +11,21 @@ import org.springframework.web.client.RestClient;
 public class RestClientConfig {
 
     @Bean
+    @Primary
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder().requestFactory(requestFactory(5_000, 5_000));
+    }
+
+    @Bean
     @LoadBalanced
     public RestClient.Builder loadBalancedRestClientBuilder() {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(2_000);
-        requestFactory.setReadTimeout(3_000);
+        return RestClient.builder().requestFactory(requestFactory(2_000, 3_000));
+    }
 
-        return RestClient.builder().requestFactory(requestFactory);
+    private SimpleClientHttpRequestFactory requestFactory(int connectTimeoutMs, int readTimeoutMs) {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(connectTimeoutMs);
+        requestFactory.setReadTimeout(readTimeoutMs);
+        return requestFactory;
     }
 }
